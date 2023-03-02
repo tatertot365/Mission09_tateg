7ef35b002be3b7375a16cafdf12b125eeb9dcdfc
tatertot365 <tate.gillespie@gmail.com>
2023-03-01 22:37:18 -0700

    first commit

---
+++ Mission09-BookStore/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mission09_BookStore.Models;
+using Mission09_BookStore.Models.ViewModels;
+
+// Tate Gillespie Section 3 group 1
+// Bookstore project
+
+namespace Mission09_BookStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        // This creates the context for the repo of books we have
+        private BookstoreBooksRepository repo;
+
+        public HomeController(ILogger<HomeController> logger, BookstoreBooksRepository temp)
+        {
+            _logger = logger;
+            repo = temp;
+        }
+
+        // This controls the index page by checking what pages the user is on and outputting the correct books
+        public IActionResult Index(int pageNum = 1)
+        {
+            int pageSize = 5;
+
+            var x = new BooksViewModel
+            {
+                Books = repo.Books
+                    .OrderBy(b => b.Title)
+                    .Skip((pageNum - 1) * pageSize)
+                    .Take(pageSize),
+
+                PageInfo = new PageInfo {
+                    TotalNumBooks = repo.Books.Count(),
+                    BooksPerPage = pageSize,
+                    CurrentPage = pageNum
+                }
+            };
+
+            return View(x);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
+
